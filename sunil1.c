e1a715f08c12b9faa468b9e5b37005822a134aa4
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-09 12:33:44 +0530

    Add files via upload

---
+++ sunil1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  void main()
+  {
+  	int a;
+  	
+  	printf(" enter a number (65-90 or 97-122)");
+  	scanf("%d",&a);
+  	
+  	printf(" Corresponding ASCII number for %d is %c",a,a);
+  	
+
+
+
+  	
+  }
